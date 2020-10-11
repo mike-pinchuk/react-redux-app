ce1c633f870ed6d201f60eb68f9da8b5e321c943
Mike <morning.sunshine.work@gmail.com>
2020-10-11 18:05:42 +0300

    Updates

---
+++ precache-manifest.1fb440a44b78624d9c345165968af753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d60c464f78c52c97c8e8b87fdb68a5",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "25279c3d92ec738a3f50",
+    "url": "/react-redux-app/static/css/main.1f94f4c6.chunk.css"
+  },
+  {
+    "revision": "58c954e0f3ba98c8ab00",
+    "url": "/react-redux-app/static/js/2.5774980a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-app/static/js/2.5774980a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25279c3d92ec738a3f50",
+    "url": "/react-redux-app/static/js/main.80d18f2e.chunk.js"
+  },
+  {
+    "revision": "8ef7322aa62dc08bee15",
+    "url": "/react-redux-app/static/js/runtime-main.cd765655.js"
+  }
+]);
